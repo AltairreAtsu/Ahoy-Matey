11776aa84bd9d0ed00a2699bd506a655a9413b5d
Marc Taurasi <altairre.atsu@gmail.com>
2018-02-15 12:19:37 -0700

    Added New Level Loader. Made Splash Screen Animation

---
+++ Assets/Utility Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip ("THe delay in seconds before loading the main menu.")]
+    private float splashDelay = 1f;
+
+    private void Start()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Invoke("LoadNextScene", splashDelay);
+        }
+    }
+
+    public void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
