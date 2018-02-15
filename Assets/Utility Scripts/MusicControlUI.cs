f4f3930d63bd16d2bae190a85b2e2c8b3564791d
Marc Taurasi <altairre.atsu@gmail.com>
2018-02-15 11:55:02 -0700

    Implemented Mute Music Button. Now possible to mute music in the Main Menu, volume graphic updates accordingly

---
+++ Assets/Utility Scripts/MusicControlUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicControlUI : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip ("Image to display while the music playing.")]
+    private Sprite unmutedImage = null;
+    [SerializeField]
+    [Tooltip ("Image to display while the music is muted.")]
+    private Sprite mutedImage = null;
+
+    private Image image = null;
+
+    private bool muted = false;
+
+	private void Start () {
+        image = GetComponent<Image>();
+	}
+
+    public void ToggleMute()
+    {
+        muted = !muted;
+        if (muted)
+        {
+            image.sprite = mutedImage;
+        }
+        else
+        {
+            image.sprite = unmutedImage;
+        }
+    }
+}
